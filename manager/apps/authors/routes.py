4291fe9ff228f980a58a644b888fc5af684ad89f
Ajay Patil <ajay.patil.ap01@gmail.com>
2020-08-10 10:11:05 +0530

    Pages created dmca privacy policy

--- manager/apps/authors/routes.py
+++ manager/apps/authors/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from sqlalchemy import func
-from manager.models.Books import Books, Authors, Tags
+from manager.models.Books import Books, Authors, Tags, Publishers
 
 authors = Blueprint('authors', __name__)
 
@@ -16,10 +16,35 @@ def author(author_id):
 								.order_by(Books.created_on.desc())\
 								.paginate(page= request.args.get('page',1,type=int), per_page=14)
 	random_authors = Authors.query.order_by(func.random()).limit(4)
-	return render_template('apps/authors/author.html', author=author, author_books=author_books, random_authors=random_authors)
+	
+	return render_template('apps/authors/author.html', author=author, author_books=author_books
+													 , random_authors=random_authors)
 
 
 @authors.route('/authors/<author_id>/follow')
 def author_follow(author_id):
 	author = Tags.query.get_or_404(author_id)
+	
 	return render_template('apps/authors/author.html', author=author)
+
+@authors.route('/publisher')
+def publishers_all():
+	publishers = Publishers.query.all()
+	
+	return render_template('apps/authors/publishers.html', publishers=publishers)
+
+@authors.route('/publishers/<publisher_id>/')
+def publisher(publisher_id):
+	publisher = Publishers.query.get_or_404(publisher_id)
+	publisher_books = publisher.books.filter(Books.post_status==0)\
+								.order_by(Books.created_on.desc())\
+								.paginate(page= request.args.get('page',1,type=int), per_page=14)
+	random_publishers = Publishers.query.order_by(func.random()).limit(4)
+
+	return render_template('apps/authors/publisher.html', publisher=publisher, publisher_books=publisher_books
+														, random_publishers=random_publishers)
+
+@authors.route('/authors/<publisher_id>/follow')
+def publisher_follow(publisher_id):
+	publisher = Publishers.query.get_or_404(publisher_id)
+	return render_template('apps/authors/publisher.html', publisher=publisher)
--- manager/apps/pages/routes.py
+++ manager/apps/pages/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from manager.models.Books import Books, Authors
 from manager.models.Users import Users
+from manager.apps.pages.forms import FeedbackForm
 
 pages = Blueprint('pages', __name__)
 
@@ -13,11 +14,16 @@ def privacy():
     return render_template('apps/pages/privacy.html')
 
 
-@pages.route('/feedback')
-def feedback():
-    return render_template('apps/pages/feedback.html')
+# @pages.route('/feedback')
+# def feedback():
+#     form = FeedbackForm()
+#     return render_template('apps/pages/feedback.html', form=form)
 
 
 @pages.route('/terms')
 def terms():
     return render_template('apps/pages/terms.html')
+
+@pages.route('/dmca')
+def dmca():
+    return render_template('apps/pages/dmca.html')
